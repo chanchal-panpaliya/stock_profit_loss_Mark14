d970e48c4890649aeec0326f1c14c424ac73dd43
Chanchal Panpaliya <chanchal.panpaliya@gmail.com>
2021-10-15 13:03:59 +0530

    commit and removed materialui mark14

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import { Component } from 'react';
 import './App.css';
-import { Button, TextField } from '@material-ui/core';
 
-import  Facebook  from '@material-ui/icons/Facebook';
-import  Twitter  from '@material-ui/icons/Twitter';
-import  LinkedIn  from '@material-ui/icons/LinkedIn';
-import  GitHub  from '@material-ui/icons/GitHub';
+import FacebookIcon from './img/icons/facebook.png';
+import TwitterIcon from './img/icons/twitter.png';
+import LinkedInIcon from './img/icons/linkedin.png';
+import GithubIcon from './img/icons/github.png'; 
 
 class App extends Component {
 
@@ -56,23 +55,29 @@ class App extends Component {
             <div className="stock_profit_loss_title">  Stock Profit and Loss Calculator </div> 
 
             <div className="stock_profit_loss_initialPrice"> 
-                <TextField type="number" placeholder="Intial Price" variant="filled" value={this.state.InitialPrice}  error={this.state.InitialPrice?this.state.InitialPrice < 1:""} helperText={ this.state.InitialPrice < 1 ? 'Min 1' : '' } onChange={(e)=>{this.setState({ InitialPrice : e.target.value ,displaymsg:"",backgroundColor:""})}}/>
+            <h5>Intial Price</h5>
+                <input type="number" placeholder="Intial Price"  value={this.state.InitialPrice} onChange={(e)=>{this.setState({ InitialPrice : e.target.value ,displaymsg:"",backgroundColor:""})}} style={{width:'20%',height:'5vh'}}/>
+                <br/>{this.state.InitialPrice < 1 ? <small style={{color:'red'}}> Min 1 </small> : ""}
             </div>
           
             <div className="stock_profit_loss_Quantity_of_Stocks"> 
-                <TextField type="number" placeholder="Quantity of Stocks" variant="filled" value={this.state.QuantityofStocks} error={this.state.QuantityofStocks?this.state.QuantityofStocks < 1:""} helperText={ this.state.QuantityofStocks < 1 ? 'Min 1' : '' } onChange={(e)=>{this.setState({ QuantityofStocks : e.target.value ,displaymsg:"",backgroundColor:""})}}/>
+            <h5>Quantity of Stocks</h5>
+                <input type="number" placeholder="Quantity of Stocks"  value={this.state.QuantityofStocks} onChange={(e)=>{this.setState({ QuantityofStocks : e.target.value ,displaymsg:"",backgroundColor:""})}} style={{width:'20%',height:'5vh'}}/>
+                <br/>{this.state.QuantityofStocks< 1 ? <small style={{color:'red'}}> Min 1 </small> : ""}
             </div>
       
             <div className="stock_profit_loss_Current_Price"> 
-                <TextField type="number" placeholder="Current Price" variant="filled" value={this.state.CurrentPrice} error={this.state.CurrentPrice?this.state.CurrentPrice < 1:""} helperText={ this.state.CurrentPrice < 1 ? 'Min 1' : '' } onChange={(e)=>{this.setState({ CurrentPrice : e.target.value ,displaymsg:"",backgroundColor:""})}}/>
+            <h5>Current Price</h5>
+                <input type="number" placeholder="Current Price"  value={this.state.CurrentPrice} onChange={(e)=>{this.setState({ CurrentPrice : e.target.value ,displaymsg:"",backgroundColor:""})}} style={{width:'20%',height:'5vh'}}/>
+                <br/>{this.state.CurrentPrice< 1 ? <small style={{color:'red'}}> Min 1 </small> : ""}
             </div>
           
             <div className="stock_profit_loss_button">
                 { this.state.InitialPrice <= 0 || this.state.QuantityofStocks <= 0 ||  this.state.CurrentPrice <= 0 || this.state.InitialPrice==="" || this.state.QuantityofStocks === "" ||  this.state.CurrentPrice === ""?
-                  <span style={{cursor:'no-drop'}}>
-                    <Button variant="contained" disabled>  Calculate </Button> 
-                  </span>:
-                  <Button variant="contained" onClick={this.CalculateProfitLoss}> Calculate </Button>
+                 
+                    <button  style={{cursor:'no-drop',width:'20%',height:'5vh',color:'black'}} disabled>  Calculate </button> 
+                  :
+                  <button  style={{width:'20%',height:'5vh',backgroundColor:'rgba(173,255,47, 0.5)'}} onClick={this.CalculateProfitLoss}> Calculate </button>
                 }
             </div>
 
@@ -95,22 +100,22 @@ class App extends Component {
             <div className="menu__socialItems">
                   <a className="menu__socialLink" target="_blank" href='https://www.facebook.com/chanchal.panpaliya'> 
                     <span className="menu__socialIcon">
-                      <Facebook  style={{color:'grey'}}/>
+                      <img style={{width:'8%',paddingTop:'2%'}} src={FacebookIcon} alt="facebooklink"/>
                     </span>
                   </a> <br/><br/>
                   <a className="menu__socialLink" target="_blank" href='https://twitter.com/CPanpaliya'> 
                      <span className="menu__socialIcon">
-                        <Twitter style={{color:'grey'}}/>
+                        <img style={{width:'8%',paddingTop:'2%'}} src={TwitterIcon} alt="twitterlink"/>
                      </span>
                   </a><br/><br/>
                   <a className="menu__socialLink" target="_blank" href='https://www.linkedin.com/in/chanchal-panpaliya-0b0436112'> 
                     <span className="menu__socialIcon">
-                      <LinkedIn style={{color:'grey'}}/> 
+                       <img style={{width:'8%',paddingTop:'2%'}} src={LinkedInIcon} alt="linkedinlink"/>
                       </span>
                   </a> <br/><br/>
                   <a className="menu__socialLink" target="_blank" href='https://github.com/chanchal-panpaliya'> 
                     <span className="menu__socialIcon">
-                      <GitHub style={{color:'grey'}}/>
+                      <img style={{width:'8%',paddingTop:'2%'}} src={GithubIcon} alt="githublink"/>
                     </span>
                 </a>
               </div>
